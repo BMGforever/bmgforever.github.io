3d2828b6d04b52deeaaf493b8e4b97e268bfbb95
BMGforever <alex.simon.1001@gmail.com>
2025-05-22 15:09:51 +0200

    Datenschutzerklärung, statischer zugriff

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import LeanCanvasPage from "./pages/LeanCanvasPage";
 import DailyAnalysisPage from "./pages/DailyAnalysisPage";
 import ScrollToTop from "./components/ScrollToTop";
 import KalorienTrackerPage from "./pages/kalorientrackerPage";
-import DatenschutzPage from "./pages/DatenschutzerklärungPage";
 
 function App() {
   return (
@@ -16,7 +15,6 @@ function App() {
         <Route path="/canvas" element={<LeanCanvasPage />} />
         <Route path="/analyse" element={<DailyAnalysisPage />} />
         <Route path="/kalorientracker" element={<KalorienTrackerPage />} />
-        <Route path="/datenschutz" element={<DatenschutzPage />} />
       </Routes>
     </Router>
   );
